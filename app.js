aa1b220f7920106924ed115608f0c7d3bdb67e76
Kanyawadee kongkaew <kanyawadeekongkaew@partykywdd.local>
2024-05-14 11:29:13 +0700

    edit js4-ex1

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const dayNumber = 6;
+
 if (dayNumber == 0) {
   console.log("Sunday");
+} else if (dayNumber == 1) {
+  console.log("Monday");
+} else if (dayNumber == 2) {
+  console.log("Tuesday");
+} else if (dayNumber == 3) {
+  console.log("Wednesday");
+} else if (dayNumber == 4) {
+  console.log("Thursday");
+} else if (dayNumber == 5) {
+  console.log("Friday");
 } else if (dayNumber == 6) {
   console.log("Saturday");
-} else {
+}else if (dayNumber == 7) {
+  console.log("Sunday");
+}else {
   console.log("Invalid day number");
 }
